81a301b6c70ec3d0745e4fa694fe20eccbceb5b9
mat <27899617+mat-1@users.noreply.github.com>
2020-11-29 13:07:29 -0600

    Add lengthof, base64, and hex
    Add lengthof, base64, and hex, and minor fixes for other engines

--- engines/answers/wikipedia.js
+++ engines/answers/wikipedia.js
@@ -1,8 +1,13 @@
 const { requestJSON } = require('../../parser')
 
+const wikipediaRegex = /^([^<>]+)$/i
+
 
 async function request(query) {
-	const summaryJson = await requestJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exintro&explaintext&redirects=1&exsentences=2&titles=' + encodeURIComponent(query))
+	const regexMatch = query.match(wikipediaRegex)
+	if (!regexMatch) return {}
+	const wikipediaQuery = regexMatch[1]
+	const summaryJson = await requestJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exintro&explaintext&redirects=1&exsentences=2&titles=' + encodeURIComponent(wikipediaQuery))
 	const pages = summaryJson.query.pages
 	const pageId = Object.keys(pages)[0]
 	const article = pages[pageId]
