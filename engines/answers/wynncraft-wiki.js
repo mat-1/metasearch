002d39a2db45a7d0338f918b6a694c1265d15d7b
mat <27899617+mat-1@users.noreply.github.com>
2020-11-29 21:02:00 -0600

    change wynncraft-wiki to only search quests

--- engines/answers/wynncraft-wiki.js
+++ engines/answers/wynncraft-wiki.js
@@ -6,7 +6,8 @@ const wynncraftRegex = /^(?:wynn(?:craft)?)?\s*?([^<>]+?)\s*(?:wynn(?:craft)?)?$
 async function request(query) {
 	const regexMatch = query.match(wynncraftRegex)
 	if (!regexMatch) return {}
-	const wynncraftQuery = regexMatch[1]
+	let wynncraftQuery = regexMatch[1] + ' (Quest)'
+	wynncraftQuery = wynncraftQuery.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
 	const summaryJson = await requestJSON('https://wynncraft.gamepedia.com/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=' + encodeURIComponent(wynncraftQuery))
 	const pages = summaryJson.query.pages
 	const pageId = Object.keys(pages)[0]
@@ -14,7 +15,7 @@ async function request(query) {
 	if (article.missing !== undefined) return {}
 	return {
 		sidebar: {
-			title: 'Wynncraft - ' + article.title,
+			title: article.title,
 			content: article.extract,
 			image: '/wynncraft.webp',
 			url: 'https://wynncraft.gamepedia.com/' + article.title.replace(' ', '_')
