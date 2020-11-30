4f2366aead1ac8e03e705bb137d0eee6ba37a657
mat <27899617+mat-1@users.noreply.github.com>
2020-11-29 21:12:57 -0600

    fix wynncraft wiki (tower of ascension not showing up)

--- engines/answers/wynncraft-wiki.js
+++ engines/answers/wynncraft-wiki.js
@@ -7,7 +7,7 @@ async function request(query) {
 	const regexMatch = query.match(wynncraftRegex)
 	if (!regexMatch) return {}
 	let wynncraftQuery = regexMatch[1] + ' (Quest)'
-	wynncraftQuery = wynncraftQuery.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
+	wynncraftQuery = wynncraftQuery.replace(/(^\w{1})|(\s+\w{1})(?=\w{2})/g, letter => letter.toUpperCase())
 	const summaryJson = await requestJSON('https://wynncraft.gamepedia.com/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=' + encodeURIComponent(wynncraftQuery))
 	const pages = summaryJson.query.pages
 	const pageId = Object.keys(pages)[0]
