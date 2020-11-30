915c02d6502cea9fe73eefae526917eb0a6d6d87
mat <27899617+mat-1@users.noreply.github.com>
2020-11-29 21:21:45 -0600

    add url(en|de)code

---
+++ engines/answers/urlcode.js
@@ -0,0 +1,25 @@
+const { requestJSON } = require('../../parser')
+
+const urlcodeRegex = /^(?:ur[li] (encode|decode) )(.+)$/i
+
+async function request(query) {
+	const regexMatch = query.match(urlcodeRegex)
+	if (!regexMatch) return {}
+	const intent = regexMatch[1].trim().toLowerCase()
+	const string = regexMatch[2].trim()
+	let answer
+	if (intent == 'encode')
+		answer = encodeURIComponent(string)
+	else if (intent == 'decode')
+		answer = decodeURIComponent(string)
+
+	if (!answer) return {}
+
+	return {
+		answer: {
+			content: answer,
+		}
+	}
+}
+
+module.exports = { request }
