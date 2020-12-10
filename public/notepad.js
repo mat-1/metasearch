4b6859027879cd54c2bf61bf4376d4966e7e8680
mat <27899617+mat-1@users.noreply.github.com>
2020-12-09 18:54:59 -0600

    fix notepad

--- public/notepad.js
+++ public/notepad.js
@@ -1,33 +1,41 @@
 let notepadContainer = document.getElementById('notepad-container')
-require([
-	'codemirror/lib/codemirror',
-	'codemirror/mode/htmlmixed/htmlmixed',
-	'codemirror/mode/python/python',
-], function(CodeMirror) {
-	let editor = CodeMirror.fromTextArea(notepadContainer, {
-		lineNumbers: true,
-		mode: 'python',
-		theme: 'default'
-	})
 
-	let pending
-	editor.on('change', function() {
-		clearTimeout(pending)
-		pending = setTimeout(update, 400)
-	})
-	function guessLanguage(code) {
-		let aliases = {
-			html: 'htmlmixed'
+function start() {
+	require([
+		'codemirror/lib/codemirror',
+		'codemirror/mode/htmlmixed/htmlmixed',
+		'codemirror/mode/python/python',
+	], function(CodeMirror) {
+		let editor = CodeMirror.fromTextArea(notepadContainer, {
+			lineNumbers: true,
+			mode: 'python',
+			theme: 'default'
+		})
+
+		let pending
+		editor.on('change', function() {
+			clearTimeout(pending)
+			pending = setTimeout(update, 400)
+		})
+		function guessLanguage(code) {
+			let aliases = {
+				html: 'htmlmixed'
+			}
+			let language = hljs.highlightAuto(code, ['javascript', 'python', 'html', 'css', 'xml']).language
+			if (aliases[language])
+				language = aliases[language]
+			return language
 		}
-		let language = hljs.highlightAuto(code, ['javascript', 'python', 'html', 'css', 'xml']).language
-		if (aliases[language])
-			language = aliases[language]
-		return language
-	}
-	function update() {
-		let code = editor.getValue()
-		let language = guessLanguage(code)
-		editor.setOption('mode', language)
-	}
-	
+		function update() {
+			let code = editor.getValue()
+			let language = guessLanguage(code)
+			editor.setOption('mode', language)
+		}
+		
+	})
+}
+if (notepadContainer) start()
+else document.addEventListener('DOMContentLoaded', () => {
+	notepadContainer = document.getElementById('notepad-container')
+	start()
 })
