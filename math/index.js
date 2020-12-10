37e67ea238a3169e1b54fa4f452993eaeeb77a11
mat <27899617+mat-1@users.noreply.github.com>
2020-12-09 18:31:49 -0600

    remove unused functions

--- math/index.js
+++ math/index.js
@@ -1,32 +1,6 @@
-var Fraction = require('./src/fractions');
-var Expression = require('./src/expressions').Expression;
-var Equation = require('./src/equations');
-var Parser = require('./src/parser');
+var Parser = require('./src/parser')
 
 
-var parse = function(input){
-	var parser = new Parser();
-	var result = parser.parse(input);
-	return result;
-};
-
-var toTex = function(input) {
-	if (input instanceof Fraction || input instanceof Expression || input instanceof Equation) {
-		return input.toTex();
-	} else if (input instanceof Array) {
-		return input.map(
-			function(e) {
-				if (e instanceof Fraction) {
-					return e.toTex();
-				} else {
-					return e.toString();
-				}
-			}
-		).join();
-	} else {
-		return input.toString();
-	}
-};
 
 function solve(input) {
 	var foundEquations = input.match(/^([xyz]|[+\-*=^\/\.]|[0-9]+| |\(|\)){3,}\b\)?$/g)
