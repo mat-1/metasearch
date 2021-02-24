dcc223f785fcc960c00c6524d333619a9fa99d35
mat <27899617+mat-1@users.noreply.github.com>
2021-02-24 00:38:24 -0600

    Create metasearch-easteregg.ts

---
+++ src/engines/answers/metasearch-easteregg.ts
@@ -0,0 +1,19 @@
+import { EngineRequest } from '../../search'
+
+// searching "metasearch" will show metasearch as the top result
+
+export async function request(query): Promise<EngineRequest> {
+	if (query.toLowerCase() === 'metasearch')
+		return {
+			results: [{
+				title: 'Metasearch',
+				position: 1,
+				content: 'The best (meta)search engine',
+				url: 'https://s.matdoes.dev',
+			}]
+		}
+	else
+		return {}
+}
+
+export const weight = 100
