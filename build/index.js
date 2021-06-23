39e600dfb5f27b76a836738b8477d6d24cd7c94a
mat <github@matdoes.dev>
2021-06-22 23:46:41 -0500

    remove sushi themes and add dracula theme

--- build/index.js
+++ build/index.js
@@ -74,8 +74,8 @@ app.get('/search', async function (req, res) {
     }
 });
 app.get('/opensearch.xml', async function (req, res) {
-    res.header('Content-Type', 'text/html');
-    res.header('Content-Disposition', 'attachment; filename="opensearch.xml"');
+    // res.header('Content-Type', 'text/html')
+    // res.header('Content-Disposition', 'attachment; filename="opensearch.xml"')
     // res.header('Content-Type', 'application/opensearchdescription+xml')
     render(res, 'opensearch.xml', {
         host: req.hostname
--- src/index.ts
+++ src/index.ts
@@ -69,8 +69,8 @@ app.get('/search', async function(req: ExpressRequest, res) {
 })
 
 app.get('/opensearch.xml', async function(req, res) {
-	res.header('Content-Type', 'text/html')
-	res.header('Content-Disposition', 'attachment; filename="opensearch.xml"')
+	// res.header('Content-Type', 'text/html')
+	// res.header('Content-Disposition', 'attachment; filename="opensearch.xml"')
 	// res.header('Content-Type', 'application/opensearchdescription+xml')
 	render(res, 'opensearch.xml', {
 		host: req.hostname
