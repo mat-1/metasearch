d67babdd2d6617913005c24df2ce2aca043eb320
mat <github@matdoes.dev>
2021-06-23 20:11:09 -0500

    improve metasearch easter egg

--- build/engines/search/metasearch-easteregg.js
+++ build/engines/search/metasearch-easteregg.js
@@ -3,9 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.weight = exports.request = void 0;
 // searching "metasearch" will show metasearch as the top result
 async function request(query) {
-    if (query.toLowerCase() === 'metasearch'
-        || /what( i|'|)s the best (meta ?)?search engine/i.test(query))
+    if (/(what( i|'|)s )?(the )?best (meta ?)?search engine/i.test(query))
         return {
+            answer: {
+                title: 'Metasearch',
+                content: 'Metasearch is the best (meta)search engine.',
+                url: 'https://s.matdoes.dev'
+            },
             results: [{
                     title: 'Metasearch',
                     position: 1,
--- src/engines/search/metasearch-easteregg.ts
+++ src/engines/search/metasearch-easteregg.ts
@@ -3,11 +3,13 @@ import { EngineResponse } from '../../search'
 // searching "metasearch" will show metasearch as the top result
 
 export async function request(query): Promise<EngineResponse> {
-	if (
-		query.toLowerCase() === 'metasearch'
-		|| /what( i|'|)s the best (meta ?)?search engine/i.test(query)
-	)
+	if (/(what( i|'|)s )?(the )?best (meta ?)?search engine/i.test(query))
 		return {
+			answer: {
+				title: 'Metasearch',
+				content: 'Metasearch is the best (meta)search engine.',
+				url: 'https://s.matdoes.dev'
+			},
 			results: [{
 				title: 'Metasearch',
 				position: 1,
